c6b536b1e9a956f35c30ad2a14553c5185648a4c
yama-chiiii <nagayama0409@gmail.com>
2025-10-17 09:56:07 +0900

    ディレクトリ構成作成

---
+++ src/app/game/page.tsx
@@ -0,0 +1,8 @@
+export default function Game() {
+  return (
+    <div className="w-full h-auto">
+      <div>game</div>
+        <div>すごろく画面</div>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignUp() {
+  return (
+    <div className="w-full h-auto">
+      <div>signup</div>
+      <div>新規登録画面</div>
+    </div>
+  );
+}
