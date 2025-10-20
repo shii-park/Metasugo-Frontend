80664812ea1f94d4c9a680f8c0a64aa81fe0b80d
yama-chiiii <nagayama0409@gmail.com>
2025-10-20 20:24:16 +0900

    背景画像配置

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
+import "./../globals.css";
+
 export default function Game() {
   return (
-    <div className="w-full h-auto">
-      <div>game</div>
-        <div>すごろく画面</div>
+    <div className="relative w-full h-screen bg-brown-light">
+      <Image
+        src="/back1.png"
+        alt=""
+        fill
+        className="object-contain z-0 pointer-events-none"
+        aria-hidden="true"
+        priority
+      />
+      <div className="relative z-10 w-full h-full flex justify-center items-center">
+        <div>
+          game
+          <div>すごろく画面</div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,ts,jsx,tsx}", // あなたの環境に合わせてパスを調整
-        "./app/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-        screens: {
-            portrait: { raw: "(orientation: portrait)" },
-            landscape: { raw: "(orientation: landscape)" },
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          DEFAULT: "#CBB994",
+          light: "#E3DECF",
         },
+        blue: {
+          DEFAULT: "#5B7BA6",
+        },
+        red: {
+          DEFAULT: "#A65B5B",
+        },
+        yellow: {
+          DEFAULT: "#DDDB6C",
+        },
+        green: {
+          DEFAULT: "#94CB99",
+        },
+        pink: {
+          DEFAULT: "#DC6BA2",
+        },
+        gray: {
+          light: "#E2DED6",
+        },
+      },
     },
-    plugins: [],
+    screens: {
+      portrait: { raw: "(orientation: portrait)" },
+      landscape: { raw: "(orientation: landscape)" },
+    },
+  },
+  plugins: [],
 };
