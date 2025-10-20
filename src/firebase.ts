2788a416cab6e56834180107631a1053518465b1
noah3139 <noah.621704@gmail.com>
2025-10-20 16:01:21 +0900

    APIキーの再設定と.envファイルの作成

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDqCUZGdgA8sh0WiakwpW7gBVhj9F4CHes",
-    authDomain: "metaversugoroku.firebaseapp.com",
-    projectId: "metaversugoroku",
-    storageBucket: "metaversugoroku.firebasestorage.app",
-    messagingSenderId: "198970298729",
-    appId: "1:198970298729:web:10bbc12c8b8bb4416fedf8",
-    measurementId: "G-R64LX4N9RX"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
