1f445252ef83e6596c0d17cd745df3a4faad5d27
noah3139 <noah.621704@gmail.com>
2025-10-22 00:25:44 +0900

    ログアウト機能の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,17 @@ import React from "react";
 import { useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useRouter } from "next/navigation";
-// ログアウト処理
-// import { logout } from "../services/authService";
+import { logout } from "../services/authService";
 
 
 export default function Home() {
   const { user, loading } = useAuth();
   const router = useRouter();
 
-  // ログアウト処理
-  // const handleLogout = async () => {
-  //   await logout();
-  //   router.push("/");
-  // };
+  const handleLogout = async () => {
+    await logout();
+    router.push("/signin");
+  };
 
   useEffect(() => {
     if (!loading &&!user) {
@@ -47,12 +45,7 @@ export default function Home() {
     //   <h1 className="text-3xl font-bold text-[#5B7BA6] mb-6">
     //     ようこそ、{user.email} さん！
     //   </h1>
-    //   <button
-    //     onClick={handleLogout}
-    //     className="bg-[#5B7BA6] text-white py-2 px-4 rounded hover:bg-[#4A6991]"
-    //   >
-    //     ログアウト
-    //   </button>
+
     // </div>
     <div>
       <div className="portrait:fixed portrait:inset-0 portrait:bg-[#5B7BA6] portrait:text-white portrait:text-2xl portrait:flex portrait:items-center portrait:justify-center portrait:z-50 portrait:overflow-hidden portrait:touch-none">
@@ -61,17 +54,31 @@ export default function Home() {
 
       <div className="fixed inset-0 flex h-[100dvh] items-center justify-center bg-black overflow-hidden">
         <div className="relative w-full h-full max-w-[177.78vh] max-h-[56.25vw] aspect-video bg-[#E3DECF] py-2 px-4">
-          <img
-            src="/logo.svg"
-            alt="ロゴ"
-            className="absolute top-3 left-10"
-          />
+          <div className="flex">
+            <img
+              src="/logo.svg"
+              alt="ロゴ"
+              className="absolute top-3 left-10"
+            />
+            <div className="flex flex-col absolute top-8 right-10">
+              <button
+                onClick={handleLogout}
+                className="text-[#5B7BA6] text-sm flex justify-end"
+              >
+                ログアウト
+              </button>
+              <div className="text-[#5B7BA6] text-xs">
+              {user.email}
+              </div>
+            </div>
+          </div>
+          
           <div className= "flex justify-center items-center border-2 border-white w-full h-full">
             <div className="relative mx-4 w-full h-hull">
-              <div className="font-bold text-3xl text-[#5B7BA6] flex items-center justify-center text-center m-8">
+              <div className="font-bold text-3xl text-[#5B7BA6] flex items-center justify-center text-center mb-4">
                 わくわく！<br />高専教員なりきりゲーム
               </div>
-              <div className="font-bold text-2xl text-[#5B7BA6] flex items-center justify-center m-8">
+              <div className="font-bold text-2xl text-[#5B7BA6] flex items-center justify-center mb-4">
                 ～お前も高専教員にならないか～
               </div>
               <div className="font-bold bg-white text-l text-[#5B7BA6] flex-col items-center justify-center px-6 py-4 w-2/5 mx-auto">
